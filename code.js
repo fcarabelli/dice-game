538cca0dde3df826ce66e6d8ffed4b0a2fa751ba
fcarabelli <42442513+fcarabelli@users.noreply.github.com>
2020-10-25 21:29:41 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,25 @@
+
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+var randomDice1 = ("images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+var randomDice2 = ("images/dice" + randomNumber2 + ".png");
+
+
+var img1 = document.querySelector(".img1");
+var img2 = document.querySelector(".img2"); 
+
+img1.setAttribute("src", randomDice1);
+img2.setAttribute("src", randomDice2);
+
+var title = document.querySelector("h1");
+
+if (randomNumber1 > randomNumber2) {
+    title.innerHTML = "🚩 Player1 Wins!"
+} else if (randomNumber1 < randomNumber2) {
+    title.innerHTML = "🚩 Player2 Wins!"
+} else {
+    title.innerHTML = "Draw!"
+}
+
+
